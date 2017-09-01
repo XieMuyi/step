25b0b23bd6ab857eb7d40c0dcf004622a298adc8
XieMuyi <xiemuyi@foxmail.com>
2017-09-01 12:41:23 +0800

    增加首页及静态资源

--- src/main/java/com/linmw/step/modules/base/controller/HelloController.java
+++ src/main/java/com/linmw/step/modules/base/controller/HelloController.java
@@ -1,17 +1,18 @@
 package com.linmw.step.modules.base.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.Map;
+
+@Controller
 public class HelloController {
 
     @RequestMapping("/")
-
     public String hello(){
-
-
-        return"Hello world!";
+        return "base/index";
 
     }
 }
